fae6643625bdfd171a8511a68666c22ad52b053a
MDey <michaeldey@hotmail.com>
2019-11-01 16:22:49 -0600

    adding pygame.quit() AFTER sys.exit() to allow game exit

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,15 +9,24 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    # Set the background color
+    bg_color = (0, 0, 230)
+
     # Start the main loop for the game.
+    count = 1
     while True:
 
         # Watch for keboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
+                
+
+        # Redraw the screen during each pass thorugh the loop.
+        screen.fill(bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
-
+        
 run_game()
