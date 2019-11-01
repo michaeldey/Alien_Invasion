3907eceefb5ea69149c4161a51e65e9919c69f08
MDey <michaeldey@hotmail.com>
2019-11-01 16:35:23 -0600

    refactoring settings to settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,15 @@
 import sys
 
 import pygame
+from settings import Settings
 
 def run_game():
+    
     #initialize game and create a screen object.
-
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Set the background color
@@ -22,7 +25,7 @@ def run_game():
                 
 
         # Redraw the screen during each pass thorugh the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
