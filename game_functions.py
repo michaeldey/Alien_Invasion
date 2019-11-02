838933c824f5aa83da89d0d49a677a1c500f8a17
MDey <michaeldey@hotmail.com>
2019-11-01 19:00:18 -0600

    refactor game_function keyup and keydown event listeners

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,26 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """ Respond to keypresses """
+
+    # move ship to the right
+    if event.key == pygame.K_RIGHT:                
+        ship.moving_right = True
+
+    # move ship to the left
+    elif event.key == pygame.K_LEFT:                
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    # stop ship if right arrow key is depressed
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+
+    # stop ship if left arrow key is depressed
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False    
+
 def check_events(ship):
     """ Respond to keypresses and mouse events """
     
@@ -11,24 +31,11 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
 
-            # move ship to the right
-            if event.key == pygame.K_RIGHT:                
-                ship.moving_right = True
-
-            # move ship to the left
-            elif event.key == pygame.K_LEFT:                
-                ship.moving_left = True
 
         elif event.type == pygame.KEYUP:
-
-            # stop ship if right arrow key is depressed
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-
-            # stop ship if left arrow key is depressed
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
